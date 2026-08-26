a6ccf95c19a70117d35303f65432666914e7b9f0
Agustin Coccia <aguscoccia1905@gmail.com>
2026-08-26 10:14:16 -0300

    Se crea los archivos para la api, y las clases Mascota, Gato y Perro

---
+++ Entities/Mascota.cs
@@ -0,0 +1,14 @@
+namespace TpApiRest;
+
+public abstract class Mascota
+{
+    private int id;
+    private string nombre;
+
+    private int edad;
+
+    public int Id {get{ return this.id; } set { this.id = value; } }
+    public string Nombre {get{ return this.nombre; } set { this.nombre = value; } }
+    public int Edad {get{ return this.edad; } set { this.edad = value; } }
+
+}
